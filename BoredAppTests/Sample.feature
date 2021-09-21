3170647f5db970c28e7af207d0258a1e0b51a320
brennan801 <brennan.hunter@gmail.com>
2021-09-20 20:28:35 -0600

    Add project files.

---
+++ BoredAppTests/Sample.feature
@@ -0,0 +1,15 @@
+﻿Feature: Sample
+	Simple calculator for adding two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the sum should be 120
+
+Scenario: Multiply two numbers
+	Given the first number is 5
+	And the second number is 4
+	When the two numbers are multiplied
+	Then the product should be 20
---
+++ BoredWebApp/Services/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoredWebApp.Services
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b; 
+        }
+        
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
