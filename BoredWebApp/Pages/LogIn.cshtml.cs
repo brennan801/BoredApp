2f7f3845fcef5ecb89eb7705cc1f1674066afb3c
brennan801 <brennan.hunter@gmail.com>
2022-02-16 21:35:06 -0700

    add a cookie yum

---
+++ BoredWebApp/Pages/LogIn.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BoredWebApp.Pages
+{
+    public class LogInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            var userName = Request.Form["userName"];
+            var password = Request.Form["password"];
+            var cookieOptions = new CookieOptions
+            {
+                // Set the secure flag, which Chrome's changes will require for SameSite none.
+                // Note this will also require you to be running on HTTPS.
+                Secure = true,
+
+                // Set the cookie to HTTP only which is good practice unless you really do need
+                // to access it client side in scripts.
+                HttpOnly = true,
+
+                // Add the SameSite attribute, this will emit the attribute with a value of none.
+                // To not emit the attribute at all set
+                // SameSite = (SameSiteMode)(-1)
+                SameSite = SameSiteMode.None
+            };
+
+            // Add the cookie to the response cookie collection
+            Response.Cookies.Append($"{userName} Cookie", "All your base are belong to us.", cookieOptions);
+        }
+    }
+}
--- BoredWebApp/Startup.cs
+++ BoredWebApp/Startup.cs
@@ -27,6 +27,14 @@ namespace BoredWebApp
             services.AddRazorPages();
             services.AddSingleton<IBoredAPIService, BoredAPIService>();
             services.AddSingleton<IDBService, DbService>();
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
