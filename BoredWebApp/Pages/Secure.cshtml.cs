b1145f27661fa4edbeec9aa1182a7df16a4244cb
brennan801 <brennan.hunter@gmail.com>
2022-02-19 19:13:23 -0700

    guatever

--- BoredWebApp/Pages/Secure.cshtml.cs
+++ BoredWebApp/Pages/Secure.cshtml.cs
@@ -1,3 +1,4 @@
+using BoredWebApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,12 @@ namespace BoredWebApp.Pages
 {
     public class SecureModel : PageModel
     {
+        private readonly IDBService dBService;
+
+        public SecureModel(IDBService dBService)
+        {
+            this.dBService = dBService;
+        }
         public string UserName { get; set; }
         public string Message { get; set; }
         public void OnGet(string UserName)
