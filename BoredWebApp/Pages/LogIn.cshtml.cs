0f71352cfa41a302d7f44cdc39d91d75e2a53523
brennan801 <brennan.hunter@gmail.com>
2022-02-19 17:39:41 -0700

    What does this do?

--- BoredWebApp/Pages/LogIn.cshtml.cs
+++ BoredWebApp/Pages/LogIn.cshtml.cs
@@ -53,30 +53,30 @@ namespace BoredWebApp.Pages
             if (oldHashed == newHashed)
             {
                 Message = "You have been logged in!";
+                var cookieOptions = new CookieOptions
+                {
+                    // Set the secure flag, which Chrome's changes will require for SameSite none.
+                    // Note this will also require you to be running on HTTPS.
+                    Secure = true,
+
+                    // Set the cookie to HTTP only which is good practice unless you really do need
+                    // to access it client side in scripts.
+                    HttpOnly = true,
+
+                    // Add the SameSite attribute, this will emit the attribute with a value of none.
+                    // To not emit the attribute at all set
+                    // SameSite = (SameSiteMode)(-1)
+                    SameSite = SameSiteMode.None
+                };
+
+                // Add the cookie to the response cookie collection
+                Response.Cookies.Append($"Cookie", "AllYourBaseAreBelongToUs", cookieOptions);
+                Message = Request.Cookies["Cookie"];
             }
             else
             {
                 Message = "Incorrect Credentials! Please Try Again!";
             }
-
-            var cookieOptions = new CookieOptions
-            {
-                // Set the secure flag, which Chrome's changes will require for SameSite none.
-                // Note this will also require you to be running on HTTPS.
-                Secure = true,
-
-                // Set the cookie to HTTP only which is good practice unless you really do need
-                // to access it client side in scripts.
-                HttpOnly = true,
-
-                // Add the SameSite attribute, this will emit the attribute with a value of none.
-                // To not emit the attribute at all set
-                // SameSite = (SameSiteMode)(-1)
-                SameSite = SameSiteMode.None
-            };
-
-            // Add the cookie to the response cookie collection
-            Response.Cookies.Append($"Cookie", "AllYourBaseAreBelongToUs", cookieOptions);
         }
     }
 }
