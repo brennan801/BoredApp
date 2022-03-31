8204ebf976fd43ac82509d6d6cf35b24b8dbbf52
brennan801 <brennan.hunter@gmail.com>
2022-03-30 23:34:46 -0600

    logout

--- BoredWebApp/Controllers/AccountController.cs
+++ BoredWebApp/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using Auth0.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -22,6 +24,28 @@ namespace BoredWebApp.Controllers
             await HttpContext.ChallengeAsync(
               Auth0Constants.AuthenticationScheme,
               authenticationProperties
+            );
+        }
+
+        [HttpGet("logout")]
+        [Authorize]
+        public async Task Logout()
+        {
+            var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+              // Indicate here where Auth0 should redirect the user after a logout.
+              // Note that the resulting absolute Uri must be added to the
+              // **Allowed Logout URLs** settings for the app.
+              .WithRedirectUri("/")
+              .Build();
+
+            // Logout from Auth0
+            await HttpContext.SignOutAsync(
+              Auth0Constants.AuthenticationScheme,
+              authenticationProperties
+            );
+            // Logout from the application
+            await HttpContext.SignOutAsync(
+              CookieAuthenticationDefaults.AuthenticationScheme
             );
         }
     }
