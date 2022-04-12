d512f28f68eb9fdad86d253ab78da38c69342a21
brennan801 <brennan.hunter@gmail.com>
2022-04-12 16:35:50 -0600

    set the host to 80 in program

--- BoredWebApp/Program.cs
+++ BoredWebApp/Program.cs
@@ -20,6 +20,7 @@ namespace BoredWebApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:80");
                     webBuilder.UseStartup<Startup>();
                 });
     }
