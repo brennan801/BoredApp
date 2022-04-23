55311b122953cd16cb5442f773fedb3cfb2783bf
brennan801 <brennan.hunter@gmail.com>
2022-04-23 14:27:47 -0600

    add auth0 secret

--- BoredWebApp/Startup.cs
+++ BoredWebApp/Startup.cs
@@ -38,7 +38,7 @@ namespace BoredWebApp
             services.AddAuth0WebAppAuthentication(options => {
                  options.Domain = Configuration["Auth0:Domain"];
                  options.ClientId = Configuration["Auth0:ClientId"];
-                options.ClientSecret = Configuration["Auth0:Secret"];
+                options.ClientSecret = Environment.GetEnvironmentVariable("auth0");
              }).WithAccessToken(options =>
              {
                  options.Audience = Configuration["Auth0:Audience"];
